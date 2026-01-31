c51ebf07777a4041ce4886078b433e78d9ec0990
cecilia chiappero <mariaceciliachiappero@gmail.com>
2026-01-31 12:37:21 -0300

    main

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('App Initialized');
+    
+    // Smooth scroll for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
